eddce6804dca25de38c367833fbb58d91cc63034
Jeremy Meyer <jjman505@gmail.com>
2014-10-28 18:19:55 -0400

    alerts, lol

--- js/init.js
+++ js/init.js
@@ -1,7 +1,27 @@
 $(function() {
-  $("#page-container").backstretch([
-      "./img/inbox.jpg"
-    , "./img/facebook.png"
-    , "./img/frustrated-woman.jpg"
+  $('#page-container').backstretch([
+      './img/inbox.jpg'
+    , './img/facebook.png'
+    , './img/frustrated-woman.jpg'
   ], {duration: 5000, fade: 1250});
+
+  $('#btn-signup').click(function(e) {
+    e.preventDefault();
+    swal({
+      title: 'Success!',
+      text: 'Account created!',
+      type: 'success',
+      confirmButtonText: 'Cool.'
+    });
+  });
+
+  $('#btn-login').click(function(e) {
+    e.preventDefault();
+    swal({
+      title: 'Success!',
+      text: 'Logged in!',
+      type: 'success',
+      confirmButtonText: 'Cool.'
+    });
+  });
 });
